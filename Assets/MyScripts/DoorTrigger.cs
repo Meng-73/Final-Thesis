c38e014940d77fb904f3772a65f6e56852fb521a
Meng-73 <119870901+Meng-73@users.noreply.github.com>
2023-11-23 22:11:33 +0000

    Update DoorTrigger.cs

--- Assets/MyScripts/DoorTrigger.cs
+++ Assets/MyScripts/DoorTrigger.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-//场景1中，门触发器，加载场景2
-//加入了黑屏过渡（淡出）
+
 public class DoorTrigger : MonoBehaviour
 {
 
-    public string sceneName = "Scene2";//定义场景
+    public string sceneName = "Scene2";
 
 
-    public Image imageEffect;//黑屏过渡的图片在Canvas中
+    public Image imageEffect;
 
-    public float speed = 1;//黑屏过渡的速度
+    public float speed = 1;
 
 
 
@@ -25,29 +24,22 @@ public class DoorTrigger : MonoBehaviour
         {
 
             FindObjectOfType<AudioManager>().PlaySound("Transfer");
-            //播放音频
-            //所有声音播放都用这句代码
+         
 
 
-
-            //黑屏透明度变化
-
-            //SceneManager.LoadScene(sceneName);
-            StartCoroutine(SceneLoadOut());//开启协程
+          
+     
+            StartCoroutine(SceneLoadOut());//start
 
             
         }
     }
 
 
-    //协程（黑屏过渡）淡出
-    //图片的透明度从0到1，如果透明度<0，则一直往上不断的加，直到透明度=1
-    //https://www.bilibili.com/video/BV1Cm4y1i7jq?p=4&spm_id_from=pageDriver&vd_source=84edfb70ef913025c87fb8c33a197d8a
 
     IEnumerator SceneLoadOut()
     {
-        //yield return null;//等待一帧
-
+       
         Color tempColor = imageEffect.color ;
         tempColor.a = 0;
         imageEffect.color = tempColor;
@@ -56,10 +48,10 @@ public class DoorTrigger : MonoBehaviour
         {
             imageEffect.color += new Color(0, 0, 0, speed * Time.deltaTime);
 
-            yield return null;//等待一帧
+            yield return null;//wait 1 second
 
         }
-        SceneManager.LoadScene(sceneName);//加载场景
+        SceneManager.LoadScene(sceneName);//load
         
     }
 
