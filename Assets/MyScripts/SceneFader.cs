a6cb54c410364900ba6a1eaa6078bc1ff7a54394
Meng-73 <119870901+Meng-73@users.noreply.github.com>
2023-11-23 22:17:48 +0000

    Update SceneFader.cs

--- Assets/MyScripts/SceneFader.cs
+++ Assets/MyScripts/SceneFader.cs
@@ -5,20 +5,19 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
-//场景2开始的黑屏过渡
-//淡入
+//scene 2 fader
 public class SceneFader : MonoBehaviour
 {
 
-    public Image imageEffect;//黑屏过渡的图片在Canvas中
+    public Image imageEffect;
 
-    public float speed = 1f;//黑屏过渡的速度
+    public float speed = 1f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SceneLoadIn());//开启淡入的协程
+        StartCoroutine(SceneLoadIn());//start
     }
 
     // Update is called once per frame
@@ -28,10 +27,10 @@ public class SceneFader : MonoBehaviour
     }
 
 
-    //图片的透明度从1到0，如果透明度>0，则一直往下不断的减，直到透明度=0
+    
     IEnumerator SceneLoadIn()
     {
-        //yield return null;//等待一帧
+        
 
         Color tempColor = imageEffect.color;
         tempColor.a = 1;
@@ -41,7 +40,7 @@ public class SceneFader : MonoBehaviour
         {
             imageEffect.color += new Color(0, 0, 0, -speed * Time.deltaTime);
 
-            yield return null;//等待一帧
+            yield return null;//wait
 
         }
        
