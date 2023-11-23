15404536e0666503773a040a65902e69231a19d2
Meng-73 <119870901+Meng-73@users.noreply.github.com>
2023-11-23 22:15:40 +0000

    Update Open.cs

--- Assets/MyScripts/Open.cs
+++ Assets/MyScripts/Open.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//点击人物打开弹窗（场景1）
+//dying man scene 1
 public class Open : MonoBehaviour
 {
-    //点击打开对话框
+    //open
     public void Click()
     {
         gameObject.SetActive(true);
