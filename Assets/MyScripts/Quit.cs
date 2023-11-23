7debef92bf056cda63e0d5a5520a3890fd88fc9c
Meng-73 <119870901+Meng-73@users.noreply.github.com>
2023-11-23 22:17:05 +0000

    Update Quit.cs

--- Assets/MyScripts/Quit.cs
+++ Assets/MyScripts/Quit.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//点击人物关闭弹窗（场景1）
+//dying man scene 1
 public class Quit : MonoBehaviour
 {
-    //退出打开对话框
+    //quit
     public void Click()
     {
         gameObject.SetActive(false);
