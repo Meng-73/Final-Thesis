0583910b04343471f1ed16a88d7daaa1b00417ef
Meng-73 <119870901+Meng-73@users.noreply.github.com>
2023-11-23 22:16:33 +0000

    Update PlayerControll.cs

--- Assets/MyScripts/PlayerControll.cs
+++ Assets/MyScripts/PlayerControll.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-//场景2中自己写的玩家（办公男）
+//scene 2 player
 public class PlayerControll : MonoBehaviour
 {
 
     private Animator animator;
 
-    public float speed = 2f; //在面板上修改速度
+    public float speed = 2f; //speed
 
 
 
@@ -24,51 +24,38 @@ public class PlayerControll : MonoBehaviour
     {
 
 
-        //水平轴
+      
         float horizontal = Input.GetAxis("Horizontal");
 
-        //垂直轴
+        
         float vertical = Input.GetAxis("Vertical");
 
-        //向量
+      
         Vector3 dir = new Vector3(horizontal, 0, vertical);
 
         
 
-        //切换动画
-        //当用户按下按键
+       
         if (dir != Vector3.zero)
         {
 
-            //面向向量
+           
             transform.rotation = Quaternion.LookRotation(dir);
 
-            //播放跑步动画
+  
             animator.SetBool("IsRun",true);
 
-            //朝向前方移动
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
       
         }else
         {
-            //播放站立动画
+  
             animator.SetBool("IsRun", false);
         
         }
 
-        //跳跃
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    animator.SetBool("IsJump", true);
-
-        //}
-        //else
-        //{
-        //    //播放站立动画
-        //    animator.SetBool("IsJump", false);
-
-        //}
+     
 
 
 
