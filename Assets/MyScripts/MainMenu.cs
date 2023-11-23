d20f6286ba822ae32ed234b613b6af44ae981f51
Meng-73 <119870901+Meng-73@users.noreply.github.com>
2023-11-23 22:13:44 +0000

    Update MainMenu.cs

--- Assets/MyScripts/MainMenu.cs
+++ Assets/MyScripts/MainMenu.cs
@@ -5,18 +5,18 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
-//开始界面，开始菜单，两个按钮
+//start panel
 public class MainMenu : MonoBehaviour
 {
-    public Image imageEffect;//黑屏过渡的图片在Canvas中
+    public Image imageEffect;
 
-    public float speed = 1;//黑屏过渡的速度
+    public float speed = 1;
 
 
     public void PlayGame()
     {
-        //SceneManager.LoadScene("SampleScene");
-        StartCoroutine(SceneLoadOut());//开启协程
+        
+        StartCoroutine(SceneLoadOut());//start
     }
 
     public void QuitGame()
@@ -27,7 +27,7 @@ public class MainMenu : MonoBehaviour
 
     IEnumerator SceneLoadOut()
     {
-        //yield return null;//等待一帧
+        
 
         Color tempColor = imageEffect.color;
         tempColor.a = 0;
@@ -37,10 +37,10 @@ public class MainMenu : MonoBehaviour
         {
             imageEffect.color += new Color(0, 0, 0, speed * Time.deltaTime);
 
-            yield return null;//等待一帧
+            yield return null;//wait 1 second
 
         }
-        SceneManager.LoadScene("SampleScene");//加载场景
+        SceneManager.LoadScene("SampleScene");//load
     }
 
 }
